1b20d691c1f839a363e2b52301bd64b5ad08d86c
Marcel Reed <reedm26@yahoo.com>
2022-04-19 15:21:36 -0600

    setting conditions for checks

---
+++ src/components/PaperworkChecks.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <div class="cardsContainer">
+      <div class="card" style="width: 18rem;" v-for="(c, index) in checks" :key="index">
+        <img :src=c.img class="card-img-top" style=" height: 11rem;;">
+        <div class="card-body">
+          <h5 class="card-title">{{ c.title }}</h5>
+          <h4 class="checkValid" :key="index" v-if="c.valid === true"> Yes </h4>
+          <h6 style="color: red" :key="index" v-if="c.valid === false"> Previous needs to be checked </h6>
+          <button v-bind="(c)" type="button" :key="index" @click="runCheck(c, index)" class="btn btn-primary">Run Check
+          </button>
+          <p>{{ index }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AirCertification from '../assets/airworthiness-cert.webp'
+import WeightBalanceCertification from '../assets/weight-and-balance.png'
+import PilotHandbook from '../assets/Pilot-Handbook.jpeg'
+import PilotLicense from '../assets/PilotLicense.jpeg'
+
+export default {
+
+  name: "PaperworkChecks",
+
+  data() {
+    return {
+      checks: [
+        {
+          id: 1,
+          title: "AirWorthiness Certification",
+          img: AirCertification,
+          valid: false
+        },
+        {
+          id: 2,
+          title: "Weight & Balance",
+          img: WeightBalanceCertification,
+          valid: false
+        },
+        {
+          id: 3,
+          title: "Handbook",
+          img: PilotHandbook,
+          valid: false
+        },
+        {
+          id: 4,
+          title: "License Valid",
+          img: PilotLicense,
+          valid: false
+        },
+
+      ],
+      notReady: true
+    }
+  },
+
+  methods: {
+    runCheck: function (checks, index) {
+      console.log(index)
+      this.validatePreviousCheck(checks, index)
+    },
+
+    validatePreviousCheck: function (checks, index) {
+      // let previousIndex = index -= 1;
+      if (index === 0) {
+        return checks.valid = !checks.valid
+      } else if (this.checks[index -= 1].valid === false) {
+        return checks.valid = false
+      } else {
+        return checks.valid = !checks.valid
+      }
+    },
+  }
+}
+
+</script>
+
+<style scoped>
+.cardsContainer {
+  display: flex;
+  justify-content: space-evenly;
+}
+
+.checkValid {
+  color: lawngreen;
+}
+
+</style>
